fe4c862c5c41f4cc2abb9038ce2aa2430712b5d3
laura-gardner <elgardner@gmail.com>
2023-03-20 14:28:49 +0000

    Add functionality to randomly select meme from array of 100 memes and display meme image

--- src/Meme.js
+++ src/Meme.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
+import memesData from './memesData.js'
+
+
+
 
 const Meme = () => {
+    
+    const [meme, setMeme] = useState({
+        topText:"", 
+        bottomText:"", 
+        randomImage:"http://i.imgflip.com/1bij.jpg"
+    })
+    const [allMemeImages, setAllMemeImages] = useState({memesData})
+    
+    const getMemeImage = () => {
+        const memeArray = allMemeImages.memesData.data.memes
+        const randomNumber = Math.floor(Math.random()*memeArray.length)
+        const url = memeArray[randomNumber].url
+        setMeme(prevMeme => ({
+                ...prevMeme, 
+                randomImage: url
+        }))
+    }
+
     return (
         <main className="meme">
-            <form className="meme--form">
+            <div className="meme--form">
                     <input 
                         className="meme--input"
                         type="text" 
@@ -14,8 +36,12 @@ const Meme = () => {
                         type="text" 
                         placeholder="Bottom text"
                     />
-                <button className="meme--button">Get a new meme image 🖼</button>
-            </form>
+                <button 
+                    className="meme--button"
+                    onClick={getMemeImage}
+                >Get a new meme image 🖼</button>
+            </div>
+            <img src={meme.randomImage} alt="" className="meme--image"/>
         </main>
     )
 }
